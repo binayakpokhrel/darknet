b3bdc5692d7b2214ba6c453fe3140346806d8ceb
binayak <binayakpokhrel@outlook.com>
2019-02-14 13:50:27 +0545

    gdrive.sh add

---
+++ gdrive.sh
@@ -0,0 +1,2 @@
+!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JFpmwqnMXvhP49OipBhf3XviZuYQED9c' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JFpmwqnMXvhP49OipBhf3XviZuYQED9c" -O yolo.weights && rm -rf /tmp/cookies.txt
+
